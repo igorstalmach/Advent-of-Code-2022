225f6459160627a75a44b1445708f9b9668681d0
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2022-12-01 22:34:30 +0100

    Updated day1 solution

--- day1/day1.py
+++ day1/day1.py
@@ -3,16 +3,23 @@ with open('day1/day1.txt', mode='r') as file:
 
 # Divide calories into lists representing elves
 data = data.split('\n\n')
-elves = []
+
+# Storing max values of calories
+max_val = [0, 0, 0]
 
 # Parse and sum values for a single elf
 for line in data:
     # Divide values into list elements (numbers are divided with '\n')
     line = line.split('\n')
-    # Map string to int and sum
-    elves.append(sum(map(int, line)))
-        
 
-elves = sorted(elves)
-print(f"Total calories of the Elf carrying the most: {elves[-1]}")
-print(f"Total calories of three of the Elves carrying the most: {sum(elves[-3:])}")
+    # Map string to int and sum
+    temp_max = sum(map(int, line))
+
+    # Compare with previous max values
+    for i in range(3):
+        if temp_max > max_val[i]:
+            max_val.insert(i, temp_max)
+            break
+ 
+print(f"Total calories of the Elf carrying the most: {max_val[0]}")
+print(f"Total calories of three of the Elves carrying the most: {sum(max_val[:3])}")
