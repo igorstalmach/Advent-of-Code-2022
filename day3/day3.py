3839b1f1a63afca2073d3d6319561c0d74386116
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2022-12-04 03:07:27 +0100

    Added day3 solution

---
+++ day3/day3.py
@@ -0,0 +1,41 @@
+def get_priority(num):
+    if ord(num) > 96:
+        return ord(num) - 96
+    else:
+        return ord(num) - 38
+
+
+def get_rucksack_total(rucksack):
+    first_compartment = set([*rucksack[:(len(rucksack)//2)]])
+    second_compartment = set(*[rucksack[(len(rucksack)//2):]])
+
+    for i in first_compartment:
+        for j in second_compartment:
+            if i == j:
+                return get_priority(i)
+
+
+def check_rucksack(rucksack1, rucksack2, rucksack3):
+    for badge in rucksack1:
+        if badge in rucksack2:
+            if badge in rucksack3:
+                return badge
+    
+
+with open('day3/day3.txt', mode='r') as file:
+    data = file.read()
+    data = data.split('\n')
+
+total1 = 0
+total2 = 0
+
+for rucksack in data:
+    total1 += get_rucksack_total(rucksack)
+
+for i in range(0, len(data), 3):
+    elf1, elf2, elf3 = data[i], data[i+1], data[i+2]
+    badge = check_rucksack(elf1, elf2, elf3)
+    total2 += get_priority(badge)
+
+print(f"Sum of the priorities for Part One: {total1}")
+print(f"Sum of the priorities for Part Two: {total2}")
